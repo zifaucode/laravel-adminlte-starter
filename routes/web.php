ef7d7dff903cc627c472d738283507f5b31fc6ab
unknown <fauziagustian68@gmail.com>
2025-01-02 22:54:31 +0700

    feat: add login page

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,47 @@
 <?php
 
 use App\Http\Controllers\web\AdminController;
+use App\Http\Controllers\web\AuthController;
+use App\Http\Controllers\web\LoginController;
 use Illuminate\Support\Facades\Route;
 
 
 /*
 |--------------------------------------------------------------------------
-| ROUTE DASHBOARD ADMIN 🟢
+| ROUTE AUTH 🟢
 |--------------------------------------------------------------------------
 */
 
-Route::controller(AdminController::class)->prefix('/')->group(function () {
-    Route::get('/', 'index')->name('dashboard');
+Route::controller(LoginController::class)->prefix('/')->group(function () {
+    Route::get('/', 'index')->name('login');
 });
+Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
 /*
 |--------------------------------------------------------------------------
-| END ROUTE FRONTEND 🟤
+| END ROUTE AUTH 🟤
+|--------------------------------------------------------------------------
+*/
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| ROUTE ADMIN 🟢
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['middleware' => 'admin'], function () {
+
+    Route::controller(AdminController::class)->prefix('/dashboard')->group(function () {
+        Route::get('/', 'index')->name('dashboard');
+    });
+});
+
+/*
+|--------------------------------------------------------------------------
+| END ADMIN 🟤
 |--------------------------------------------------------------------------
 */
